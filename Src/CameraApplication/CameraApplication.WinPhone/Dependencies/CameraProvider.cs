2633eefe1287bee59e0951c68d8833764fbe39cf
Jeppe Andersen <jeppe@outlook.com>
2015-04-21 07:28:30 +0200

    Added Windows Phone implementation of ICameraProvider

---
+++ Src/CameraApplication/CameraApplication.WinPhone/Dependencies/CameraProvider.cs
@@ -0,0 +1,52 @@
+﻿using CameraApplication.WinPhone.Dependencies;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(CameraProvider))]
+namespace CameraApplication.WinPhone.Dependencies
+{
+    using System.Threading.Tasks;
+
+    using CameraApplication.Common;
+
+    using Microsoft.Phone.Tasks;
+
+    public class CameraProvider : ICameraProvider
+    {
+        private TaskCompletionSource<CameraResult> tcs;
+
+        public Task<CameraResult> TakePictureAsync()
+        {
+            CameraCaptureTask cameraCaptureTask = new CameraCaptureTask();
+            cameraCaptureTask.Completed += CameraCaptureTaskOnCompleted;
+
+            cameraCaptureTask.Show();
+
+            tcs = new TaskCompletionSource<CameraResult>();
+
+            return tcs.Task;
+        }
+
+        private void CameraCaptureTaskOnCompleted(object sender, PhotoResult photoResult)
+        {
+            if (photoResult.TaskResult == TaskResult.None)
+            {
+                tcs.TrySetException(photoResult.Error);
+                return;
+            }
+
+            if (photoResult.TaskResult == TaskResult.Cancel)
+            {
+                tcs.TrySetResult(null);
+                return;
+            }
+
+            
+            CameraResult result = new CameraResult();
+            result.Picture = ImageSource.FromStream(() => photoResult.ChosenPhoto);
+            result.FileUri = photoResult.OriginalFileName;
+
+            tcs.TrySetResult(result);
+        }
+    }
+}
