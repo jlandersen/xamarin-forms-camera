be40022d1eb6a6e3bc9bb7a4588483748da47893
Jeppe Andersen <jeppe@outlook.com>
2015-04-21 08:08:47 +0200

    Added iOS implementation of ICameraProvider

---
+++ Src/CameraApplication/CameraApplication.iOS/Dependencies/CameraProvider.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms;
+using CameraApplication.iOS;
+
+[assembly: Dependency(typeof(CameraProvider))]
+namespace CameraApplication.iOS
+{
+	using System;
+	using System.IO;
+	using System.Threading.Tasks;
+
+	using UIKit;
+	using Foundation;
+
+	using CameraApplication.Common;
+
+	public class CameraProvider : ICameraProvider
+	{
+		public Task<CameraResult> TakePictureAsync()
+		{
+			var tcs = new TaskCompletionSource<CameraResult>();
+
+			Camera.TakePicture(UIApplication.SharedApplication.KeyWindow.RootViewController, (obj) => {
+				var photo = obj.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
+
+				// You can also get photo meta data with using the following
+				// var meta = obj.ValueForKey(new NSString("UIImagePickerControllerMediaMetadata")) as NSDictionary;
+
+				var documentsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+				string jpgFilename = Path.Combine(documentsDirectory, Guid.NewGuid().ToString() + ".jpg");
+				NSData imgData = photo.AsJPEG();
+				NSError err = null;
+
+				if (imgData.Save(jpgFilename, false, out err)) 
+				{
+					CameraResult result = new CameraResult();
+					result.Picture = ImageSource.FromStream(imgData.AsStream);
+					result.FileUri = jpgFilename;
+
+					tcs.TrySetResult(result);
+				} 
+				else 
+				{
+					tcs.TrySetException(new Exception(err.LocalizedDescription));
+				}
+			});
+
+			return tcs.Task;
+		}
+	}
+}
