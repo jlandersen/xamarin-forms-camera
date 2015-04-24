9e91a694818e4d168006b3666ecce4c49db65251
Jeppe Andersen <jeppe@outlook.com>
2015-04-24 12:57:16 +0200

    Initial implementation for Android. Because kicking off the ActionImageCapture intent returns in the OnActivityResult of MainActivity, this currently notifies the CameraProvider when the result is ready.

---
+++ Src/CameraApplication/CameraApplication.Droid/Dependencies/CameraProvider.cs
@@ -0,0 +1,60 @@
+﻿using CameraApplication.Droid.Dependencies;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(CameraProvider))]
+namespace CameraApplication.Droid.Dependencies
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Android.App;
+    using Android.Content;
+    using Android.Provider;
+
+    using CameraApplication.Common;
+
+    using Java.IO;
+
+    using Environment = Android.OS.Environment;
+    using Uri = Android.Net.Uri;
+
+    public class CameraProvider : ICameraProvider
+    {
+        private static File file;
+        private static File pictureDirectory;
+
+        private static TaskCompletionSource<CameraResult> tcs; 
+
+        public Task<CameraResult> TakePictureAsync()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+            pictureDirectory = new File(Environment.GetExternalStoragePublicDirectory(Environment.DirectoryPictures), "CameraAppDemo");
+
+            if (!pictureDirectory.Exists())
+            {
+                pictureDirectory.Mkdirs();
+            }
+
+            file = new File(pictureDirectory, String.Format("photo_{0}.jpg", Guid.NewGuid()));
+
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(file));
+
+            var activity = (Activity)Forms.Context;
+            activity.StartActivityForResult(intent, 0);
+
+            tcs = new TaskCompletionSource<CameraResult>();
+            return tcs.Task;
+        }
+
+        public static void OnResult()
+        {
+            CameraResult res = new CameraResult();
+            res.Picture = ImageSource.FromFile(file.Path);
+            res.FullFilePath = file.Path; 
+
+            tcs.TrySetResult(res);
+        }
+    }
+}
--- Src/CameraApplication/CameraApplication.Droid/MainActivity.cs
+++ Src/CameraApplication/CameraApplication.Droid/MainActivity.cs
@@ -9,15 +9,30 @@ using Android.OS;
 
 namespace CameraApplication.Droid
 {
+    using Android.Content;
+    using Android.Graphics;
+    using Android.Net;
+
+    using CameraApplication.Droid.Dependencies;
+
     [Activity(Label = "CameraApplication", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            RequestWindowFeature(WindowFeatures.ActionBar);
+
             base.OnCreate(bundle);
+
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            CameraProvider.OnResult();
         }
     }
 }
